e88010961b80546b6a23e748837f1bcd850fd346
jerrylai <jerrylai@aliyun.com>
2021-07-30 11:13:19 +0800

    yh

--- src/Afx.RabbitMQ/JsonUtils.cs
+++ src/Afx.RabbitMQ/JsonUtils.cs
@@ -343,7 +343,7 @@ namespace Afx.RabbitMQ.Json
         /// <param name="options"></param>
         public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value.ToString());
+            writer.WriteNumberValue(value);
         }
     }
     /// <summary>
--- version.cmd
+++ version.cmd
@@ -1,3 +1,3 @@
 @echo off
-set v=3.2.1
+set v=3.2.2
 tool\EditVersion dir="%cd%" v=%v% a="Assembly.cs"
